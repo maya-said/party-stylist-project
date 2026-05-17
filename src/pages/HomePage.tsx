5b3fbccf504469afd3955294cf39965c8ecc6abf
maya <mayasaid225@gmail.com>
2026-05-17 12:08:47 +0300

    test tailwind styling on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Party Stylist
+      </h1>
     </div>
   );
 };
