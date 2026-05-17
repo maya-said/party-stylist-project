2828c81fc9cee85b0588a047962a00fe5ebf807a
maya <mayasaid225@gmail.com>
2026-05-17 12:43:54 +0300

    create products and contact pages

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,15 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
+
 import HomePage from "../pages/HomePage";
+import ProductsPage from "../pages/ProductsPage";
+import ContactPage from "../pages/ContactPage";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route element={<MainLayout />}>
+
           <Route path="/" element={<HomePage />} />
+
+          <Route
+            path="/products"
+            element={<ProductsPage />}
+          />
+
+          <Route
+            path="/contact"
+            element={<ContactPage />}
+          />
+
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
