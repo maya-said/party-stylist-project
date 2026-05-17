74da61384e8d9a664a9ce80c42db462d93999882
maya <mayasaid225@gmail.com>
2026-05-17 12:26:51 +0300

    create reusable navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md px-8 py-4">
+      <h1 className="text-2xl font-bold">
+        Party Stylist
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
